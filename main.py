27f3848cecfac4f272d906114e3ae2c06edfe5bc
dmitriy.tarasov <dmitriy.tarasov@givc.mkrf.ru>
2024-12-19 16:47:04 +0300

    Init back

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from typing import List
 
 app = FastAPI()
 
+# Менеджер подключений
+class ConnectionManager:
+    def init(self):  # Исправлено: добавлены двойные подчёркивания
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        print("Подключение установлено")
+        self.active_connections.append(websocket)
+        print(f"Активные подключения: {len(self.active_connections)}")
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+manager = ConnectionManager()
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()  # Получаем сообщение от клиента
+            await manager.broadcast(f"Сообщение от клиента: {data}")  # Шлём сообщение всем
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
 
 @app.get("/")
 async def root():
