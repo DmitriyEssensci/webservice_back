0238b1e0d7b733abecf686ffd5d745684a1293df
dmitriy.tarasov <dmitriy.tarasov@givc.mkrf.ru>
2024-12-22 03:02:53 +0300

    Create delete user functional

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import engine, SessionLocal, Base
 from models import User, UserCreate
 from typing import List
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import ValidationError
-from fastapi import HTTPException
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Разрешённый адрес фронтенда
+    allow_origins=["http://localhost:3000"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Зависимость для получения сессии базы данных
 def get_db():
     db = SessionLocal()
     try:
@@ -30,8 +27,6 @@ async def root():
     return {"message": "Hello World"}
 
 Base.metadata.create_all(bind=engine)
-
-# POST-запрос для добавления нового пользователя 
 @app.post("/users/")
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
     try:
@@ -42,3 +37,18 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
         return {"message": "User added successfully", "user": {"id": new_user.id, "full_name": new_user.full_name}}
     except Exception as e:
         raise HTTPException(status_code=422, detail=str(e))
+    
+@app.get("/users/")
+def get_users(db: Session = Depends(get_db)):
+    users = db.query(User).all()
+    return {"users": users}
+
+@app.delete("/users/{user_id}")
+async def delete_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
+    db.delete(user)
+    db.commit()
+    
+    return {"message": "Пользователь удален"}
